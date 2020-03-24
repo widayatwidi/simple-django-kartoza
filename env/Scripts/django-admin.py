386c72ba8b6856f9b420481c8129e04caf343d5d
Widi Widayat <widi.widayat@mail.ugm.ac.id>
2020-03-24 11:34:43 +0700

    first app initialize - add environment and main project

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\xampp\htdocs\_python\_django\simple-django-kartoza\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
