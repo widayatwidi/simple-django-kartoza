322859207806d59368d955695da7afc887992b8b
Widi Widayat <widi.widayat@mail.ugm.ac.id>
2020-03-25 03:00:59 +0700

    add login page template

--- kartoza/kartoza/urls.py
+++ kartoza/kartoza/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', views.home, name='home-page'),
 ]
