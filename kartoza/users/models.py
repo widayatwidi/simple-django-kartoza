bc3cd4a9c316491988473278cd24a097225a7ce3
Widi Widayat <widi.widayat@mail.ugm.ac.id>
2020-03-25 08:05:38 +0700

    update - extend user model to details profile - home address, location, phone and image profile

--- kartoza/users/models.py
+++ kartoza/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	image = models.ImageField(default='profile.png', upload_to='profile_pics')
+	phone = models.CharField(max_length=30)
+	location = models.CharField(max_length=30, blank=True)
+	homeAddress = models.TextField(max_length=500, blank=True)
+
+	def __str__(self):
+		return f'{self.user.username} Profile'
