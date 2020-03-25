1636fc99864db0a08ee71dc34b977b3ad7d3a7a0
Widi Widayat <widi.widayat@mail.ugm.ac.id>
2020-03-26 04:12:32 +0700

    update - remove setting postgis database

--- kartoza/kartoza/settings.py
+++ kartoza/kartoza/settings.py
@@ -81,14 +81,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'gis',
-        'USER': 'user001',
-        'PASSWORD': '123456789',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
 }
 
 
