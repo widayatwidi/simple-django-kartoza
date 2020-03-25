f8b371ae88093dc44c7c0d1663e048e6fe94721e
Widi Widayat <widi.widayat@mail.ugm.ac.id>
2020-03-26 04:07:22 +0700

    add - start trying to create maps with geodjango package - setting up environment first

--- kartoza/kartoza/settings.py
+++ kartoza/kartoza/settings.py
@@ -80,6 +80,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gis',
+        'USER': 'user001',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
